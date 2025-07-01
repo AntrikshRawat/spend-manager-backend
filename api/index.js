441fdf69233baec0c74f274a3e19fb10406f010f
Antriksh rawat <antrikshrawat2@gmail.com>
2025-07-01 13:19:27 +0530

    ain

--- api/index.js
+++ api/index.js
@@ -5,42 +5,33 @@ const account = require("../Account");
 const payment = require("../Payment");
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
-const serverless = require("serverless-http");
+
 
 const app = express();
-
+const serverless = require("serverless-http");
 app.use(cors({
-  origin: ["http://localhost:5173"],
-  credentials: true
+  origin: "*", 
+  credentials: true           
 }));
+
+
+connectToDB();
+app.use(express.json()); // For parsing application/json
+app.use(express.urlencoded({ extended: true })); // For parsing application
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-let isDBConnected = false;
+app.get("/",(req,res)=>{
+  res.send("hello and welcome to spend-manager-api!")
+})
 
-// app.use(async (req, res, next) => {
-//   try {
-//     if (!isDBConnected) {
-//       await connectToDB();
-//       isDBConnected = true;
-//       console.log("✅ MongoDB Connected!");
-//     }
-//     next(); // ✅ CRITICAL!
-//   } catch (err) {
-//     console.error("❌ MongoDB connection error:", err);
-//     return res.status(500).json({ message: "DB connection failed" });
-//   }
-// });
+app.listen(3000,()=>{
+  console.log("server is running on 3000")
+})
 
-
-app.get("/", (req, res) => {
-  res.send("hello and welcome to spend-manager-api!");
-});
-
-// app.use("/auth/v1", auth);
-// app.use("/account", account);
-// app.use("/payment", payment);
+app.use("/auth/v1",auth);
+app.use("/account",account);
+app.use("/payment",payment);
 
 
 module.exports = serverless(app);
