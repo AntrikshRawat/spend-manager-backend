d0c1973090044ae8f0213690927629315f788469
Antriksh rawat <antrikshrawat2@gmail.com>
2025-07-01 18:59:31 +0530

    ain

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(cors({
-  origin: ["https://spend-manager-f.vercel.app/","http://localhost:5173/"], 
+  origin: ["https://spend-manager-f.vercel.app","http://localhost:5173"], 
   credentials: true       
 }));
 
