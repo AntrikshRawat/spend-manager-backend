64ce0c743cf141302b5669cd098c438af9a57355
Antriksh rawat <antrikshrawat2@gmail.com>
2025-07-01 12:12:01 +0530

    vercel setup

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const connectToDB = require('./config/database');
-const auth = require("./Auth");
-const account = require("./Account");
-const payment = require("./Payment");
+const connectToDB = require('../config/database');
+const auth = require("../Auth");
+const account = require("../Account");
+const payment = require("../Payment");
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const serverless = require("serverless-http");
 app.use(cors({
   origin: ["http://localhost:5173"], 
   credentials: true           
@@ -28,3 +28,6 @@ app.get('/', (req, res) => {
 app.use("/auth/v1",auth);
 app.use("/account",account);
 app.use("/payment",payment);
+
+
+module.exports.handler = serverless(app);
