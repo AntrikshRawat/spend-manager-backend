0599a9f2709f25bca9b8d303d96128f212d99380
Antriksh rawat <antriksh.rawat@aitner.ai>
2025-08-21 19:31:29 +0530

    update

--- Auth/logOut.js
+++ Auth/logOut.js
@@ -6,18 +6,25 @@
 
 const express = require("express");
 const Router = express.Router();
+const verifyUser = require("../Middleware/verifyUser");
+const User = require("../Models/User");
 
-Router.post("/",async(req,res)=>{
- try{
- res.clearCookie("authToken",{
-  httpOnly:true,
-  sameSite:'none',
-  secure:true,
- })
- return res.json({message:"logout Successfully!"});
-}catch(e) {
- res.status(500).json({message:"Internal Application Error"});
-}
-})
+Router.post("/", verifyUser, async (req, res) => {
+  const userId = req.userId;
+  try {
+    User.findByIdAndDelete(userId, {
+      pushSubscription: null,
+    });
+  } catch (e) {
+    res.status(500).json({ message: "Internal Application Error" });
+    return;
+  }
+  res.clearCookie("authToken", {
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  });
+  return res.json({ message: "logout Successfully!" });
+});
 
 module.exports = Router;
